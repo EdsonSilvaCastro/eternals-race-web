f67fb9a883f1db8526e5af146c7586fcf6c362a4
EdsonSilvaCastro <edsonsilvacastro89@hotmail.com>
2021-12-06 17:56:46 -0600

    Back-end working

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var cors = require("cors");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var port = 3000;
+var port = 8080;
+
 
 var app = express();
+
 
 var User = new mongoose.Schema(
   {
@@ -23,7 +25,7 @@ var User = new mongoose.Schema(
       type: String,
     },
     fechaNacimiento: {
-      type: Date,
+      type: String,
     },
     genero: {
       type: String,
@@ -69,6 +71,20 @@ var User = new mongoose.Schema(
     },
     privacy: {
       type: String,
+    },
+    responsal:
+    {
+      type:String,
+    },
+    paymentSatuts:
+    {
+      type: String,
+    },
+    validator: {
+      type: Boolean,
+    },
+    idCompra:{
+      type: String
     }
   },
   {
@@ -79,7 +95,7 @@ var User = new mongoose.Schema(
 mongoose.Promise = global.Promise;
 var Model = mongoose.model("Model", User);
 
-mongoose.connect("mongodb://sharkathon.one:27015/participantes",{
+mongoose.connect("mongodb://sharkathon.one:27015/cuponPruebas",{
   auth: {
       username:'mongo',
       password:'Jafra2018!'
@@ -99,6 +115,7 @@ mongoose.connect("mongodb://sharkathon.one:27015/participantes",{
 
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
+const { response } = require("express");
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -106,13 +123,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Agrega credenciales
 mercadopago.configure({
   access_token:
-    "APP_USR-6478429900085566-113022-63306cae1bda1b35da01505c058e6c25-153057812",
+    //"APP_USR-6478429900085566-113022-63306cae1bda1b35da01505c058e6c25-153057812",
+    "TEST-6478429900085566-113022-088e337cb14fed28c6c30a47f9a16330-153057812"
 });
 
 app.listen(port, () => {
   console.log("PORT connected: " + port);
 });
 
+//End point para redireccionar
 app.post("/checkout", (req, res) => {
   
   var myData = new Model(req.body);
@@ -126,22 +145,34 @@ app.post("/checkout", (req, res) => {
     });
 
 //Creamos el item para que mercado libre pueda crear el
-  let preference = {
-    items: [
-      {
-        title: 'Inscripcion Carrera',
-        unit_price: parseInt(req.body.tickets),
-        quantity: 1,
-      },
-    ],
-  };
+
+
+let preference = {
+  items: [
+    {
+      title: 'Inscripcion Carrera',
+      unit_price: parseInt(req.body.tickets),
+      quantity: 1,
+    }
+  ],
+  back_urls: {
+    "success": "http://localhost:8080/mensaje.html",
+    "failure": "http://localhost:8080/feedback",
+    "pending": "http://localhost:8080/feedback"
+  },
+  auto_return: "approved"
+};
 
   mercadopago.preferences
     .create(preference)
     .then(function (response) {
+    // console.log(response.body);
+
       res.redirect(response.body.init_point);
     })
     .catch(function (error) {
       console.log(error);
     });
 });
+
+
